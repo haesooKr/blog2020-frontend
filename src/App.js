3e27a989e804d5ace5b6be9fc2472692267f3048
haesooKr <gotn676@gmail.com>
2020-07-02 20:30:36 -0400

    Connect pages to router

--- src/App.js
+++ src/App.js
@@ -1,26 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Route } from 'react-router-dom';
+import PostListPage from './pages/PostListPage';
+import LoginPage from './pages/LoginPage';
+import RegisterPage from './pages/RegisterPage';
+import WritePage from './pages/WritePage';
+import PostPage from './pages/PostPage';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Route path={['/@:username', '/']} component={PostListPage} exact></Route>
+      <Route path="/login" component={LoginPage}></Route>
+      <Route path="/register" component={RegisterPage}></Route>
+      <Route path="/write" component={WritePage}></Route>
+      <Route path="/@:username/:postId" component={PostPage}></Route>
+    </>
   );
-}
+};
 
 export default App;
