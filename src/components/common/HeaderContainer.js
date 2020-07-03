eebe7835238310cc0806e4141f18815f25b96b94
haesooKr <gotn676@gmail.com>
2020-07-03 16:18:42 -0400

    Pass onLogout to Header

--- src/components/common/HeaderContainer.js
+++ src/components/common/HeaderContainer.js
@@ -1,12 +1,17 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import Header from './Header';
+import { logout } from '../../modules/user';
 
 const HeaderContainer = () => {
   const { user } = useSelector(({ user }) => ({
     user: user.user,
   }));
-  return <Header user={user} />;
+  const dispatch = useDispatch();
+  const onLogout = () => {
+    dispatch(logout());
+  };
+  return <Header user={user} onLogout={onLogout} />;
 };
 
 export default HeaderContainer;
