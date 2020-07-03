c6b788aa5977f64801e7a8e2a633c34f6a9005b8
haesooKr <gotn676@gmail.com>
2020-07-03 15:21:04 -0400

    Control Login Errors

--- src/containers/auth/LoginForm.js
+++ src/containers/auth/LoginForm.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { withRouter } from 'react-router-dom';
 import { changeField, initializeForm, login } from '../../modules/auth';
@@ -6,6 +6,7 @@ import AuthForm from '../../components/auth/AuthForm';
 import { check } from '../../modules/user';
 
 const LoginForm = ({ history }) => {
+  const [error, setError] = useState(null);
   const dispatch = useDispatch();
   const { form, auth, authError, user } = useSelector(({ auth, user }) => ({
     form: auth.login,
@@ -39,6 +40,7 @@ const LoginForm = ({ history }) => {
     if(authError){
       console.log('오류 발생');
       console.log(authError);
+      setError('로그인 실패');
       return;
     }
     if(auth){
@@ -59,6 +61,7 @@ const LoginForm = ({ history }) => {
       form={form}
       onChange={onChange}
       onSubmit={onSubmit}
+      error={error}
     />
   );
 };
