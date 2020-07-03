dc1d33df75a276b61d98e2fb545ec11aa9dd60d8
haesooKr <gotn676@gmail.com>
2020-07-03 16:01:52 -0400

    Get user as prop

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -31,15 +31,29 @@ const Spacer = styled.div`
   height: 4em;
 `;
 
-const Header = () => {
+const UserInfo = styled.div`
+  font-weight: 800;
+  margin-right: 1rem;
+`;
+
+const Header = ({ user }) => {
   return (
     <>
       <HeaderBlock>
         <Wrapper>
-          <Link to="/" className="logo">REACTLOG</Link>
-          <div className="right">
-            <Button to="/login">로그인</Button>
-          </div>
+          <Link to="/" className="logo">
+            REACTLOG
+          </Link>
+          {user ? (
+            <div className="right">
+              <UserInfo>{user.username}</UserInfo>
+              <Button to="/login">로그아웃</Button>
+            </div>
+          ) : (
+            <div className="right">
+              <Button to="/login">로그인</Button>
+            </div>
+          )}
         </Wrapper>
       </HeaderBlock>
       <Spacer />
