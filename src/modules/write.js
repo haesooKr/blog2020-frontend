be0e3934e8e56da0f1145c2ed10dcfe6179b5c2f
haesooKr <gotn676@gmail.com>
2020-07-03 18:04:32 -0400

    Add writePost saga

--- src/modules/write.js
+++ src/modules/write.js
@@ -1,18 +1,37 @@
 import { createAction, handleActions } from 'redux-actions';
+import createRequestSaga, {
+  createRequestActionTypes
+} from '../lib/createRequestSaga';
+import * as postsAPI from '../lib/api/posts';
+import { takeLatest } from 'redux-saga/effects';
 
 const INITIALIZE = 'write/INITIALIZE';
 const CHANGE_FILED = 'write/CHANGE_FIELD';
+const [ WRITE_POST, WRITE_POST_SUCCESS, WRITE_POST_FAILURE ] = createRequestActionTypes("/write/WRITE_POST");
+
 
 export const initialize = createAction(INITIALIZE);
 export const changeField = createAction(CHANGE_FILED, ({ key, value }) => ({
   key,
   value,
 }));
+export const writePost = createAction(WRITE_POST, ({title, body, tags}) => ({
+  title,
+  body,
+  tags,
+}))
+
+const writePostSaga = createRequestSaga(WRITE_POST, postsAPI.writePost);
+export function* writeSaga() {
+  yield takeLatest(WRITE_POST, writePostSaga);
+}
 
 const initialState = {
   title: '',
   body: '',
   tags: [],
+  post: null,
+  postError: null,
 };
 
 const write = handleActions(
@@ -21,6 +40,19 @@ const write = handleActions(
     [CHANGE_FILED]: (state, { payload: { key, value } }) => ({
       ...state,
       [key]: value,
+    }),
+    [WRITE_POST]: state => ({
+      ...state,
+      post: null,
+      postError: null,
+    }),
+    [WRITE_POST_SUCCESS]: (state, { payload: post }) => ({
+      ...state,
+      post
+    }),
+    [WRITE_POST_FAILURE]: (state, { dispatch: postError }) => ({
+      ...state,
+      postError
     }),
   },
   initialState,
