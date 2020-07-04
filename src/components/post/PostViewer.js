3aaa58fd3ce83579fb9cd9233defc23474761563
haesooKr <gotn676@gmail.com>
2020-07-03 23:09:41 -0400

    Update PostViewer with props

--- src/components/post/PostViewer.js
+++ src/components/post/PostViewer.js
@@ -48,26 +48,35 @@ const PostContent = styled.div`
   color: ${palette.gray[8]};
 `;
 
-const PostViewer = () => {
+const PostViewer = ({ post, error, loading }) => {
+  if (error) {
+    if (error.response && error.response.status === 404) {
+      return <PostViewerBlock>존재하지 않는 포스트입니다.</PostViewerBlock>;
+    }
+    return <PostViewerBlock>오류 발생!</PostViewerBlock>;
+  }
+  if (loading || !post) {
+    return null;
+  }
+
+  const { title, body, user, publishedData, tags } = post;
   return (
     <PostViewerBlock>
       <PostHead>
-        <h1>제목</h1>
+        <h1>{title}</h1>
         <SubInfo>
           <span>
-            <b>tester</b>
+            <b>{user.username}</b>
           </span>
-          <span>{new Date().toLocaleDateString()}</span>
+          <span>{new Date(publishedData).toLocaleDateString()}</span>
         </SubInfo>
         <Tags>
-          <div className="tag">#태그</div>
-          <div className="tag">#태그</div>
-          <div className="tag">#태그</div>
+          {tags.map((tag) => (
+            <div className="tags">#{tag}</div>
+          ))}
         </Tags>
       </PostHead>
-      <PostContent
-        dangerouslySetInnerHTML={{ __html: '<p>HTML<b>내용</b>입니다.</p>' }}
-      />
+      <PostContent dangerouslySetInnerHTML={{ __html: body }} />
     </PostViewerBlock>
   );
 };
