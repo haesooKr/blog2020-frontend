be33a7e8fef7bd45da882bea3f6464b9fb6ea8c6
haesooKr <gotn676@gmail.com>
2020-07-03 21:36:26 -0400

    Create post and postSaga

---
+++ src/modules/posts.js
@@ -0,0 +1,43 @@
+import { createAction, handleActions } from 'react-redux';
+import createRequestSaga, {
+  createRequestActionTypes,
+} from '../lib/createRequestSaga';
+import * as postsAPI from '../lib/api/posts';
+import { takeLatest } from 'redux-saga';
+
+const [
+  READ_POST,
+  READ_POST_SUCCESS,
+  READ_POST_FAILURE,
+] = createRequestActionTypes('posts/READ_POST');
+const UNLOAD_POST = 'post/UNLOAD_POST';
+
+export const readPost = createAction(READ_POST, (id) => id);
+export const unloadPost = createAction(UNLOAD_POST);
+
+const readPostSaga = createRequestSaga(READ_POST, postsAPI.readPost);
+export function* postSaga() {
+  yield takeLatest(READ_POST, readPostSaga);
+}
+
+const initialState = {
+  post: null,
+  error: null,
+};
+
+const post = handleActions(
+  {
+    [READ_POST_SUCCESS]: (state, { payload: post }) => ({
+      ...state,
+      post,
+    }),
+    [READ_POST_FAILURE]: (state, { payload: error }) => ({
+      ...state,
+      error,
+    }),
+    [UNLOAD_POST]: () => initialState,
+  },
+  initialState,
+);
+
+export default post;
